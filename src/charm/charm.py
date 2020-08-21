024f680654766e3cd02ce5b75db5f11af50f839a
Mark S. Maglana <mmaglana@gmail.com>
2020-08-21 10:15:02 +0800

    Add comments on how to define hooks

--- src/charm/charm.py
+++ src/charm/charm.py
@@ -8,6 +8,19 @@ from unboxed.dispatcher import dispatch
 log = logging.getLogger()
 
 
+# To have a hook be called by Juju, just define a function with the pattern
+# `hook_<HOOKNAME>` where `<HOOKNAME>` is one of the hooks defined in this post
+# https://discourse.juju.is/t/charm-hooks/1040 but with hyphens converted
+# to underscores.
+#
+# Each hook will be passed a `CharmContext` instance which should (eventually)
+# provide just enough information for it to do its job. See src/unboxed for
+# more information on `CharmContext`.
+#
+# Other than that, it's all just Python. You can add 3rd-party libraries
+# by adding them to the `runtime_requirements` list in `setup.py`.
+
+
 def hook_config_changed(context):
     log.debug(f"Hello from {context.hook_name}")
 
