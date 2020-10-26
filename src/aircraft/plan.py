65415d0639b76ae4dea5943dd1a8bc2e016e86ec
Mark S. Maglana <mmaglana@gmail.com>
2020-10-26 17:46:33 +0800

    Add support for v1beta1 api

--- src/aircraft/plan.py
+++ src/aircraft/plan.py
@@ -4,3 +4,6 @@ class Plan:
         self.api_version = api_version
         self.start_at = start_at
         self.rules = rules
+
+    def get_driver(self):
+        pass
--- tests/aircraft/plan_test.py
+++ tests/aircraft/plan_test.py
@@ -1,4 +1,4 @@
-from aircraft import Plan
+from aircraft import Plan, PlanDriverV1Beta1
 
 
 def test_init():
@@ -34,3 +34,16 @@ def test_init():
     assert plan.api_version == api_version
     assert plan.start_at == waypoint_a
     assert plan.rules == rules
+
+
+def test_load_correct_plan_driver():
+
+    api_version = "plan/v1beta1"
+
+    plan = Plan(
+        api_version=api_version,
+        start_at=object(),
+        rules={}
+    )
+
+    type(plan.get_driver()) == PlanDriverV1Beta1
