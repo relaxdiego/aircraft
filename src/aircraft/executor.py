0b58e8fd029b76402edce9a73e139c784b51cb71
Mark S. Maglana <mmaglana@gmail.com>
2020-10-27 13:05:11 +0800

    Raise helpful error messages on execution error

--- src/aircraft/executor.py
+++ src/aircraft/executor.py
@@ -15,7 +15,40 @@ class Executor:
 
         while wp is not None:
             log.debug("Approaching Waypoint: {}".format(wp.__name__))
-            event, data = wp(data)
+            result = wp(data)
+
+            if result is None:
+                raise WaypointDidNotReturnTuple(wp)
+
+            event, data = result
+
             log.debug("Event occured: {}".format(event))
-            log.debug("Data received: {}".format(event))
-            wp = self.__rules.get(wp, {}).get(event, None)
+            log.debug("Data received: {}".format(data))
+
+            next_rules = self.__rules.get(wp)
+            if next_rules == {}:
+                break
+
+            next_wp = next_rules.get(event, None)
+
+            if next_wp is None:
+                raise CannotTransitionError(wp, event)
+                break
+
+            wp = next_wp
+
+        return data
+
+
+class CannotTransitionError(Exception):
+
+    def __init__(self, wp, event):
+        msg = "Cannot find next waypoint from waypoint '{}' when event '{}' occurs"
+        super().__init__(msg.format(wp.__name__, event))
+
+
+class WaypointDidNotReturnTuple(Exception):
+
+    def __init__(self, wp):
+        msg = "Waypoint '{}' was excepted to return str, data but returned None"
+        super().__init__(msg.format(wp.__name__))
