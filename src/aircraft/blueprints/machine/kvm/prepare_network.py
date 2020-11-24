361216c47c975477e9eebea30021523862a74abb
Mark S. Maglana <mmaglana@gmail.com>
2020-11-24 12:38:29 +0800

    Complete the configuration of KVM host networking

--- src/aircraft/blueprints/machine/kvm/prepare_network.py
+++ src/aircraft/blueprints/machine/kvm/prepare_network.py
@@ -1,16 +1,38 @@
 from pathlib import Path
 
 from pyinfra.api import deploy
-from pyinfra.operations import files
+from pyinfra.operations import (
+    files,
+    server,
+)
 
 
-@deploy('Prepare KVM host network', data_defaults={})
+@deploy('machine.kvm.prepare_network', data_defaults={})
 def main(state, host):
 
     files.template(
-        name='Render netplan configuration',
+        name='Render netplan config',
         src=str(Path(__file__).parent / 'templates' / 'netplan.yml.j2'),
-        dest=str(Path('/tmp') / 'netplan.yml'),
+        dest=host.fact.find_files('/etc/netplan/*.y*ml')[0],
+        sudo=True,
+
+        state=state,
+        host=host,
+    )
+
+    server.shell(
+        name='Run netplan generate',
+        commands=['netplan generate'],
+        sudo=True,
+
+        state=state,
+        host=host,
+    )
+
+    server.shell(
+        name='Run netplan apply',
+        commands=['netplan apply'],
+        sudo=True,
 
         state=state,
         host=host,
