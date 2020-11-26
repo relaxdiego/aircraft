cda7ab3ad47bc8801f195bce984888cc8922d784
Mark S. Maglana <mmaglana@gmail.com>
2020-11-26 15:20:24 +0800

    Rename deployment model namespace to deployspec

---
+++ src/aircraft/models/deployspec/inventory/__init__.py
@@ -0,0 +1,16 @@
+from importlib import import_module
+
+from pydantic import BaseModel
+
+
+def load(d):
+    inventory_spec = GenericInventorySpec(**d)
+    module = import_module('aircraft.models.deployspec.inventory.'
+                           f'{inventory_spec.kind}.{inventory_spec.api_version}')
+    return module.main(d)
+
+
+class GenericInventorySpec(BaseModel):
+    kind: str
+    api_version: str
+    spec: dict
