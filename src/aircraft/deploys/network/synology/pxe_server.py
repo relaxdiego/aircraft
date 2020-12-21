edf07f3dd1d3ef59ec3ab3fd884d60271a95aa17
Mark S. Maglana <mmaglana@gmail.com>
2020-12-21 09:52:20 +0800

    Move templates and add user-data

--- src/aircraft/deploys/network/synology/pxe_server.py
+++ src/aircraft/deploys/network/synology/pxe_server.py
@@ -18,12 +18,20 @@ def configure(state=None, host=None):
 
     validate_schema_version(host.data.pxe, supported_schema_versions)
 
-    src = Path(__file__).parent / 'templates' / 'grub.cfg.j2'
-    dest = host.data.pxe['sftp_rootdir'] / 'grub' / 'grub.cfg'
+    local_base_path = Path(__file__).parent / 'templates' / \
+        host.data.pxe['schema_version']
+    remote_base_path = host.data.pxe['sftp_rootdir']
 
     files.template(
-        src=str(src),
-        dest=str(dest),
+        src=str(local_base_path / 'grub.cfg.j2'),
+        dest=str(remote_base_path / 'grub' / 'grub.cfg'),
+        create_remote_dir=False,
+        host=host, state=state,
+    )
+
+    files.template(
+        src=str(local_base_path / 'user-data.j2'),
+        dest=str(remote_base_path / 'user-data'),
         create_remote_dir=False,
         host=host, state=state,
     )
