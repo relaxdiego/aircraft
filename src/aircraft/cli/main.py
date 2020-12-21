5d024eb66ef3f45ba50527e15c0505eaff4f9f68
Mark S. Maglana <mmaglana@gmail.com>
2020-12-21 17:32:15 +0800

    Remove unused files

--- src/aircraft/cli/main.py
+++ src/aircraft/cli/main.py
@@ -1,8 +1,4 @@
-import os
-
 import click
-
-from aircraft import aircraft_dir
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -11,53 +7,3 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
 def main():
     pass
-
-
-@main.command()
-@click.argument('deployspec',
-                type=click.Path(file_okay=False,
-                                dir_okay=True,
-                                exists=True,
-                                resolve_path=True,
-                                readable=True))
-@click.option('-v', '--verbose', count=True,
-              help="Print meta (-v), input (-vv) and output (-vvv)")
-def apply(deployspec, verbose):
-    """
-    Applies DEPLOYSPEC where DEPLOYSPEC is a directory containing files
-    that lists the host inventory, deployment-specific data, and the
-    operations that must be executed against the deployment. DEPLOYSPEC
-    may either be a relative or absolute path.
-    """
-    os.environ['AIRCRAFT_DEPLOYSPEC'] = deployspec
-
-    verbosity = ''
-    if verbose > 0:
-        verbosity = f"-{'v' * verbose}"
-
-    old_path = os.getcwd()
-    os.chdir(aircraft_dir / 'deployment')
-    os.system(f"pyinfra {verbosity} inventory.py operations.py")
-    os.chdir(old_path)
-
-
-@main.command()
-@click.argument('deployspec',
-                type=click.Path(file_okay=False,
-                                dir_okay=True,
-                                exists=True,
-                                resolve_path=True,
-                                readable=True))
-def debug(deployspec):
-    """
-    Prints out inventory and oeration debug information for DEPLOYSPEC
-    and then exits. It will connect to the hosts to be able to generate
-    the oeprations list but it will not execute any of them.
-    """
-    os.environ['AIRCRAFT_DEPLOYSPEC'] = deployspec
-
-    old_path = os.getcwd()
-    os.chdir(aircraft_dir / 'deployment')
-    os.system("pyinfra inventory.py debug-inventory")
-    os.system("pyinfra --debug-operations inventory.py operations.py")
-    os.chdir(old_path)
