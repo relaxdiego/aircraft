d104610eced154d9919460ea4af8cb533a42da98
Mark S. Maglana <mmaglana@gmail.com>
2021-01-01 19:54:03 +0800

    Initial work on testing StorageConfigData

---
+++ tests/aircraft/deploys/ubuntu/models/v1beta1/storage_config_data_test.py
@@ -0,0 +1,59 @@
+import pytest
+
+from aircraft.deploys.ubuntu.models.v1beta1 import StorageConfigData
+
+
+@pytest.fixture
+def input_config(request):
+    marker = request.node.get_closest_marker('data_kwargs')
+    config = dict(
+        disks=[
+            {
+                'path': '/dev/sda',
+                'partitions': [
+                    {
+                        'size': 536870912,  # 512MB
+                        'format': 'fat32',
+                        'mount_path': '/boot/efi',
+                        'flag': 'boot',
+                        'grub_device': True,
+                    },
+                    {
+                        'size': 1073741824,  # 1GB
+                        'format': 'ext4',
+                        'mount_path': '/boot',
+                    },
+                    {
+                        'id': 'partition-for-ubuntu-vg',
+                        'size': 429496729600,  # 400GB
+                    },
+                ],
+            },
+        ],
+        lvm_volgroups=[
+            {
+                'name': 'ubuntu-vg',
+                'devices': [
+                    'partition-for-ubuntu-vg'
+                ],
+                'logical_volumes': marker.kwargs.get(
+                    'logical_volumes',
+                    [
+                        {
+                            'name': 'ubuntu-lv',
+                            'size': 397284474880,  # 370GB
+                            'format': 'ext4',
+                            'mount_path': '/',
+                        }
+                    ]
+                )
+            }
+        ]
+    )
+
+    return config
+
+
+@pytest.mark.data_kwargs(logical_volumes=[])
+def test__does_not_error_out_when_lvm_lvs_are_empty(input_config):
+    assert StorageConfigData(**input_config).export_lvm_logical_volumes() == []
