3c666d15926407c155d8e87fa4fd04b6988aa9b3
Mark S. Maglana <mmaglana@gmail.com>
2021-02-22 19:56:00 +0800

    Allow client recognition via MAC address

--- src/aircraft/deploys/ubuntu/models/v1beta3/bootfile_data.py
+++ src/aircraft/deploys/ubuntu/models/v1beta3/bootfile_data.py
@@ -1,7 +1,11 @@
 from pydantic import (
     AnyUrl,
+    root_validator,
+    validator,
 )
+import re
 from typing import (
+    List,
     Union,
 )
 
@@ -15,7 +19,8 @@ class BootfileData(BaseModel):
     client architectures, see https://tools.ietf.org/html/rfc4578#section-2.1
     """
     client_name: str
-    client_arch: Union[int, str]
+    client_arch: Union[int, str] = None
+    dhcp_macs: List[str] = None
     image_source_url: AnyUrl
     image_sha256sum: str
 
@@ -27,3 +32,19 @@ class BootfileData(BaseModel):
         DHCP Option 67 (Bootfile-Name)
         """
         return self.image_source_url.path.lstrip('/')
+
+    @validator('dhcp_macs', each_item=True)
+    def ensure_each_mac_is_valid_and_uses_colon_hexadecimal_notation(cls, value):
+        valid_pattern = r'^[0-9a-f]{2}(:[0-9a-f]{2}){2}(:([0-9a-f]{2}|\*)){3}$'
+        if re.match(valid_pattern, value) is None:
+            raise ValueError(f'{value} does not use a valid colon-hex '
+                             'mac address notation')
+        return value
+
+    @root_validator
+    def ensure_client_arch_xor_dhcp_mac_is_defined(cls, values):
+        if values.get('client_arch') is None and \
+           values.get('dhcp_macs') is None:
+            raise ValueError('Either client_arch or dhcp_mac must be defined')
+
+        return values
--- tests/aircraft/deploys/ubuntu/models/v1beta3/bootfile_data_test.py
+++ tests/aircraft/deploys/ubuntu/models/v1beta3/bootfile_data_test.py
@@ -42,3 +42,86 @@ class BootFileDataTest(TestCase):
             image_source_url="http://some.domain.com/some/path",
             image_sha256sum="someshahere",
         )
+
+    def test__it_requires_client_arch_xor_dhcp_macs_to_be_set(self):
+        # Neither client_arc nor dhcp_macs are set
+        with pytest.raises(ValueError):
+            BootfileData(
+                client_name="some-client",
+                image_source_url="http://some.domain.com/some/path",
+                image_sha256sum="someshahere",
+            )
+
+        # client_arc is set
+        BootfileData(
+            client_name="some-client",
+            client_arch='some-client-arch-substring',
+            image_source_url="http://some.domain.com/some/path",
+            image_sha256sum="someshahere",
+        )
+
+        # dhcp_mac is set
+        BootfileData(
+            client_name="some-client",
+            dhcp_macs=['b8:27:eb:*:*:*', 'dc:a6:32:*:*:*', 'e4:5f:01:*:*:*'],
+            image_source_url="http://some.domain.com/some/path",
+            image_sha256sum="someshahere",
+        )
+
+    def test__it_requires_colon_hexadecimal_notation_for_dhcp_macs(self):
+        with pytest.raises(ValueError):
+            BootfileData(
+                client_name="some-client",
+                dhcp_macs=['b8:27:eb:not:a:mac:address'],
+                image_source_url="http://some.domain.com/some/path",
+                image_sha256sum="someshahere",
+            )
+
+    def test__accepts_valid_macs_using_colon_hex_notation(self):
+        valid_macs = ['b8:27:eb:dc:a6:32', 'e4:5f:01:aa:bb:cc']
+
+        data = BootfileData(
+            client_name="some-client",
+            dhcp_macs=valid_macs,
+            image_source_url="http://some.domain.com/some/path",
+            image_sha256sum="someshahere",
+        )
+
+        assert data.dhcp_macs == valid_macs
+
+    def test__accepts_macs_with_wildcards_for_last_3_octets(self):
+        valid_macs = ['b8:27:eb:*:*:*', 'e4:5f:01:*:*:*']
+
+        data = BootfileData(
+            client_name="some-client",
+            dhcp_macs=valid_macs,
+            image_source_url="http://some.domain.com/some/path",
+            image_sha256sum="someshahere",
+        )
+
+        assert data.dhcp_macs == valid_macs
+
+    def test__it_allows_a_single_wildcard_for_each_of_the_last_3_octets(self):
+        with pytest.raises(ValueError):
+            BootfileData(
+                client_name="some-client",
+                dhcp_macs=['b8:27:eb:**:*:*'],
+                image_source_url="http://some.domain.com/some/path",
+                image_sha256sum="someshahere",
+            )
+
+        with pytest.raises(ValueError):
+            BootfileData(
+                client_name="some-client",
+                dhcp_macs=['b8:27:eb:*:**:*'],
+                image_source_url="http://some.domain.com/some/path",
+                image_sha256sum="someshahere",
+            )
+
+        with pytest.raises(ValueError):
+            BootfileData(
+                client_name="some-client",
+                dhcp_macs=['b8:27:eb:*:*:**'],
+                image_source_url="http://some.domain.com/some/path",
+                image_sha256sum="someshahere",
+            )
